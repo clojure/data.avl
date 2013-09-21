c29fedd735ae008de570e250f792d0c74a0d909d
Michał Marczyk <michal.marczyk@gmail.com>
2013-09-22 00:50:44 +0200

    More tests

--- test/clj/avl/clj_test.clj
+++ test/clj/avl/clj_test.clj
@@ -2,6 +2,9 @@
   (:use clojure.test)
   (:require [avl.clj :as avl]))
 
+(defn twice [x]
+  [x x])
+
 (def ks   (range 100000))
 (def ksks (doall (interleave ks ks)))
 (def ks'  (doall (map first (partition 2 ks))))
@@ -12,10 +15,19 @@
 (def rb-set  (apply sorted-set ks))
 (def avl-set (apply avl/sorted-set ks))
 
+(def rb-map-by->  (apply sorted-map-by > ksks))
+(def avl-map-by-> (apply avl/sorted-map-by > ksks))
+
+(def rb-set-by->  (apply sorted-set-by > ks))
+(def avl-set-by-> (apply avl/sorted-set-by > ks))
+
 (deftest sanity-checks
   (testing "AVL collections look like regular sorted collections"
     (is (= rb-map avl-map))
     (is (= rb-set avl-set)))
+  (testing "AVL collections with custom comparators looks like regular ones"
+    (is (= rb-map-by-> avl-map-by->))
+    (is (= rb-set-by-> avl-set-by->)))
   (testing "AVL collection seqs look like regular sorted collection seqs"
     (is (= (seq rb-map) (seq avl-map)))
     (is (= (seq rb-set) (seq avl-set)))
@@ -32,12 +44,27 @@
     (is (= (reduce disj rb-set ks')
            (persistent! (reduce disj! (transient avl-set) ks')))))
   (testing "*-by seqs look like they should"
+    (is (= (seq rb-map-by->) (seq avl-map-by->)))
+    (is (= (seq (apply avl/sorted-set-by > (range 32))))))
+  (testing "reduce-kv returns correct values"
+    (is (= (reduce-kv + 0 rb-map) (reduce-kv + 0 avl-map)))
+    (is (= (reduce-kv + 0 rb-map-by->) (reduce-kv + 0 avl-map-by->)))))
+
+(deftest standalone-checks
+  (testing "seq"
+    (is (= (seq avl-map) (map twice ks)))
+    (is (= (seq avl-set) ks)))
+  (testing "seq on *-by"
     (is (= (seq (apply avl/sorted-map-by > (interleave (range 32) (range 32))))
            (reverse (map (juxt identity identity) (range 32)))))
-    (is (= (seq (apply avl/sorted-set-by > (range 32)))))))
+    (is (= (seq (apply avl/sorted-set-by > (range 32)))
+           (reverse (range 32))))))
 
 (deftest rank-queries
   (testing "map rank queries work as expected"
-    (is (every? true? (map = ks (map #(key (nth avl-map %)) ks)))))
+    (is (every? true? (map = ks (map #(key (nth avl-map %)) ks))))
+    (is (every? true?
+                (map = (reverse ks) (map #(key (nth avl-map-by-> %)) ks)))))
   (testing "set rank queries work as expected"
-    (is (every? true? (map = ks (map #(nth avl-set %) ks))))))
+    (is (every? true? (map = ks (map #(nth avl-set %) ks))))
+    (is (every? true? (map = (reverse ks) (map #(nth avl-set-by-> %) ks))))))
