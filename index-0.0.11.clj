4531af42a399dc4ba5cb5250516b13a253dc0878
Tom Faulhaber <git_net@infolace.com>
2014-01-05 08:46:01 -0800

    Initial autodoc commit for data.avl

---
+++ index-0.0.11.clj
@@ -0,0 +1,126 @@
+{:namespaces
+ ({:source-url
+   "https://github.com/clojure/data.avl/blob/538c20791dd6a1c1b0fd3a677da934d7b1464207/src/main/clojure/clojure/data/avl.clj",
+   :wiki-url
+   "http://clojure.github.com/data.avl/clojure.data.avl-api.html",
+   :name "clojure.data.avl",
+   :author "Michał Marczyk",
+   :doc
+   "An implementation of persistent sorted maps and sets based on AVL\ntrees with API mimicking that of Clojure's sorted maps and\nsets (based on Red-Black Trees). Additionally, the provided map and\nset types support the transients API and logarithmic time rank\nqueries via clojure.core/nth (select element by rank) and\nclojure.data.avl/rank-of (discover rank of element)."}),
+ :vars
+ ({:arglists ([coll x]),
+   :name "rank-of",
+   :namespace "clojure.data.avl",
+   :source-url
+   "https://github.com/clojure/data.avl/blob/538c20791dd6a1c1b0fd3a677da934d7b1464207/src/main/clojure/clojure/data/avl.clj#L1285",
+   :raw-source-url
+   "https://github.com/clojure/data.avl/raw/538c20791dd6a1c1b0fd3a677da934d7b1464207/src/main/clojure/clojure/data/avl.clj",
+   :wiki-url
+   "http://clojure.github.com/data.avl//clojure.data.avl-api.html#clojure.data.avl/rank-of",
+   :doc "Returns the rank of x in coll or -1 if not present.",
+   :var-type "function",
+   :line 1285,
+   :file "src/main/clojure/clojure/data/avl.clj"}
+  {:arglists ([& keyvals]),
+   :name "sorted-map",
+   :namespace "clojure.data.avl",
+   :source-url
+   "https://github.com/clojure/data.avl/blob/538c20791dd6a1c1b0fd3a677da934d7b1464207/src/main/clojure/clojure/data/avl.clj#L1251",
+   :raw-source-url
+   "https://github.com/clojure/data.avl/raw/538c20791dd6a1c1b0fd3a677da934d7b1464207/src/main/clojure/clojure/data/avl.clj",
+   :wiki-url
+   "http://clojure.github.com/data.avl//clojure.data.avl-api.html#clojure.data.avl/sorted-map",
+   :doc
+   "keyval => key val\nReturns a new AVL map with supplied mappings.",
+   :var-type "function",
+   :line 1251,
+   :file "src/main/clojure/clojure/data/avl.clj"}
+  {:arglists ([comparator & keyvals]),
+   :name "sorted-map-by",
+   :namespace "clojure.data.avl",
+   :source-url
+   "https://github.com/clojure/data.avl/blob/538c20791dd6a1c1b0fd3a677da934d7b1464207/src/main/clojure/clojure/data/avl.clj#L1260",
+   :raw-source-url
+   "https://github.com/clojure/data.avl/raw/538c20791dd6a1c1b0fd3a677da934d7b1464207/src/main/clojure/clojure/data/avl.clj",
+   :wiki-url
+   "http://clojure.github.com/data.avl//clojure.data.avl-api.html#clojure.data.avl/sorted-map-by",
+   :doc
+   "keyval => key val\nReturns a new sorted map with supplied mappings, using the supplied\ncomparator.",
+   :var-type "function",
+   :line 1260,
+   :file "src/main/clojure/clojure/data/avl.clj"}
+  {:arglists ([& keys]),
+   :name "sorted-set",
+   :namespace "clojure.data.avl",
+   :source-url
+   "https://github.com/clojure/data.avl/blob/538c20791dd6a1c1b0fd3a677da934d7b1464207/src/main/clojure/clojure/data/avl.clj#L1272",
+   :raw-source-url
+   "https://github.com/clojure/data.avl/raw/538c20791dd6a1c1b0fd3a677da934d7b1464207/src/main/clojure/clojure/data/avl.clj",
+   :wiki-url
+   "http://clojure.github.com/data.avl//clojure.data.avl-api.html#clojure.data.avl/sorted-set",
+   :doc "Returns a new sorted set with supplied keys.",
+   :var-type "function",
+   :line 1272,
+   :file "src/main/clojure/clojure/data/avl.clj"}
+  {:arglists ([comparator & keys]),
+   :name "sorted-set-by",
+   :namespace "clojure.data.avl",
+   :source-url
+   "https://github.com/clojure/data.avl/blob/538c20791dd6a1c1b0fd3a677da934d7b1464207/src/main/clojure/clojure/data/avl.clj#L1277",
+   :raw-source-url
+   "https://github.com/clojure/data.avl/raw/538c20791dd6a1c1b0fd3a677da934d7b1464207/src/main/clojure/clojure/data/avl.clj",
+   :wiki-url
+   "http://clojure.github.com/data.avl//clojure.data.avl-api.html#clojure.data.avl/sorted-set-by",
+   :doc
+   "Returns a new sorted set with supplied keys, using the supplied comparator.",
+   :var-type "function",
+   :line 1277,
+   :file "src/main/clojure/clojure/data/avl.clj"}
+  {:file nil,
+   :raw-source-url nil,
+   :source-url nil,
+   :wiki-url
+   "http://clojure.github.com/data.avl//clojure.data.avl-api.html#clojure.data.avl/AVLMap",
+   :namespace "clojure.data.avl",
+   :var-type "type",
+   :name "AVLMap"}
+  {:file nil,
+   :raw-source-url nil,
+   :source-url nil,
+   :wiki-url
+   "http://clojure.github.com/data.avl//clojure.data.avl-api.html#clojure.data.avl/AVLMapSeq",
+   :namespace "clojure.data.avl",
+   :var-type "type",
+   :name "AVLMapSeq"}
+  {:file nil,
+   :raw-source-url nil,
+   :source-url nil,
+   :wiki-url
+   "http://clojure.github.com/data.avl//clojure.data.avl-api.html#clojure.data.avl/AVLNode",
+   :namespace "clojure.data.avl",
+   :var-type "type",
+   :name "AVLNode"}
+  {:file nil,
+   :raw-source-url nil,
+   :source-url nil,
+   :wiki-url
+   "http://clojure.github.com/data.avl//clojure.data.avl-api.html#clojure.data.avl/AVLSet",
+   :namespace "clojure.data.avl",
+   :var-type "type",
+   :name "AVLSet"}
+  {:file nil,
+   :raw-source-url nil,
+   :source-url nil,
+   :wiki-url
+   "http://clojure.github.com/data.avl//clojure.data.avl-api.html#clojure.data.avl/AVLTransientMap",
+   :namespace "clojure.data.avl",
+   :var-type "type",
+   :name "AVLTransientMap"}
+  {:file nil,
+   :raw-source-url nil,
+   :source-url nil,
+   :wiki-url
+   "http://clojure.github.com/data.avl//clojure.data.avl-api.html#clojure.data.avl/AVLTransientSet",
+   :namespace "clojure.data.avl",
+   :var-type "type",
+   :name "AVLTransientSet"})}
