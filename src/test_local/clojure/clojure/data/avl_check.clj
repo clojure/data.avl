245c5bd4d9b100ee38d67179e3cb17d6f2baae43
Michał Marczyk <michal.marczyk@gmail.com>
2014-01-17 23:24:49 +0100

    Randomized tests validating tree structure

--- src/test_local/clojure/clojure/data/avl_check.clj
+++ src/test_local/clojure/clojure/data/avl_check.clj
@@ -12,3 +12,39 @@
   (are [x] (assert-set-like x igen)
     (avl/sorted-set)
     (avl/sorted-set-by >)))
+
+(defn validate-tree
+  ([tree]
+     (if (or (map? tree) (set? tree))
+       (do
+         (assert (== (count tree)
+                     (peek (validate-tree
+                            (.comparator ^clojure.lang.Sorted tree)
+                            (.getTree ^clojure.data.avl.IAVLTree tree)))))
+         true)
+       (validate-tree clojure.lang.RT/DEFAULT_COMPARATOR tree)))
+  ([^java.util.Comparator comp ^clojure.data.avl.IAVLNode tree]
+     (if (nil? tree)
+       [0 0]
+       (let [left      (.getLeft tree)
+             right     (.getRight tree)
+             [lh lcnt] (validate-tree comp left)
+             [rh rcnt] (validate-tree comp right)
+             h         (inc (max lh rh))]
+         (if left
+           (assert (neg? (.compare comp (.getKey left) (.getKey tree)))))
+         (if right
+           (assert (neg? (.compare comp (.getKey tree) (.getKey right)))))
+         (assert (#{-1 0 1} (- lh rh)))
+         (assert (== lh (#'avl/height left)))
+         (assert (== rh (#'avl/height right)))
+         (assert (== lcnt (.getRank tree)))
+         (assert (== h (.getHeight tree)))
+         [h (inc (+ lcnt rcnt))]))))
+
+(deftest avl-invariant
+  (testing "AVL invariant is maintained when inserting keys in random order"
+    (dotimes [_ 500]
+      (let [ks (shuffle (range 500))]
+        (validate-tree (apply avl/sorted-set ks))
+        (validate-tree (apply avl/sorted-set-by > ks))))))
