64153074512abd0a09f1b6d2c8247bb808c0ec15
Alex Miller <alex.miller@cognitect.com>
2024-02-19 10:18:38 -0600

    update parent pom and versions

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.6.1"
-  :parent [org.clojure/pom.contrib "0.1.2"]
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :parent [org.clojure/pom.contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :jvm-opts ^:replace ["-Dorg.clojure.data.avl.test.large-tree-size=100000"
                        "-Dorg.clojure.data.avl.test.medium-tree-size=100000"
                        "-Dorg.clojure.data.avl.test.small-tree-size=300"]
@@ -13,9 +13,9 @@
   :test-paths ["src/test/clojure"]
   :aliases {"all" ["with-profile" "dev:dev,1.6:dev,1.7:dev,1.8:dev,1.9"]}
   :profiles {:dbg  {:jvm-opts ["-XX:-OmitStackTraceInFastThrow"]}
-             :cljs {:dependencies [[org.clojure/clojure "1.10.0"]
+             :cljs {:dependencies [[org.clojure/clojure "1.10.3"]
                                    [org.clojure/clojurescript "1.10.520"]
-                                   [org.clojure/test.check "0.9.0"]
+                                   [org.clojure/test.check "1.1.1"]
                                    [collection-check "0.1.7"]]
                     :hooks [leiningen.cljsbuild]
                     :plugins [[lein-cljsbuild "1.1.4"]]
@@ -31,16 +31,7 @@
                                           :optimizations :advanced
                                           :pretty-print false
                                           :static-fns true}}}}}
-             :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]
-                                   [org.clojure/test.check "0.9.0"]
-                                   [collection-check "0.1.7"]]
-                    :test-paths ["src/test_local/cljc"]}
-             :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]
-                                   [org.clojure/test.check "0.9.0"]
-                                   [collection-check "0.1.7"]]
-                    :test-paths ["src/test_local/cljc"]}
-             :1.9  {:dependencies [[org.clojure/clojure "1.9.0-beta2"]
-                                   [org.clojure/test.check "0.9.0"]
+             :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]
+                                   [org.clojure/test.check "1.1.1"]
                                    [collection-check "0.1.7"]]
                     :test-paths ["src/test_local/cljc"]}})
